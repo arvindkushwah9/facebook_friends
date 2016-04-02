3cea10af4c96997592f045caa26af88eaebcf504
Mattjames <mattjamesywr@gmail.com>
2016-04-02 15:14:30 +0530

    Return al json

--- lib/facebook_friends.rb
+++ lib/facebook_friends.rb
@@ -6,13 +6,12 @@ module FacebookFriends
     # Your code goes here...
     def self.count(token)
       # TODO: process `str`
-      return "Helllow"
-      # response = Net::HTTP.get_response(URI("https://graph.facebook.com/v2.5/me/friends?access_token=#{token}"))
-      #   unless response.is_a?(Net::HTTPSuccess)
-      #     return nil
-      #   end
-      #   json = ActiveSupport::JSON.decode(response.body)
-      #   json['summary']['total_count'].to_s  + ' Friends'
+        response = Net::HTTP.get_response(URI("https://graph.facebook.com/v2.5/me/friends?access_token=#{token}"))
+        unless response.is_a?(Net::HTTPSuccess)
+          return nil
+        end
+        json = ActiveSupport::JSON.decode(response.body)
+        return json
     end
   end
 end
